1eae63f0075cabaefd38613e3e931de8d46c72de
Michael Ramassamy <ramassamy2000@gmail.com>
2017-05-17 14:43:44 +0200

    Added geocoding to Gear model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516164235) do
+ActiveRecord::Schema.define(version: 20170517123408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20170516164235) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "size_id"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["size_id"], name: "index_gears_on_size_id", using: :btree
     t.index ["user_id"], name: "index_gears_on_user_id", using: :btree
   end
