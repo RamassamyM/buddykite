3810ed25272a1f061c3230f5db3973fe58238ef3
christian villaescusa <christian.villaescusa@gmail.com>
2017-05-17 18:03:21 +0200

    Adding map and booking button in show

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -11,6 +11,12 @@ class GearsController < ApplicationController
 
   def show
     @gear = Gear.find(params[:id])
+    @user = User.find(@gear.user_id)
+    # @gear = Gear.where.not(latitude: nil, longitude: nil)
+    @hash = Gmaps4rails.build_markers(@gear) do |gear, marker|
+      marker.lat gear.latitude
+      marker.lng gear.longitude
+    end
   end
 
   private
