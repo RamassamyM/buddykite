8f4a473a676c0f790f85abb51c515e331c10e4b1
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-05-17 15:24:18 +0200

    navbar integration done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,28 @@
 module ApplicationHelper
   def remove_footer_from_home
-    "footer-hidden" if is_home
+    "hidden" if is_home
   end
 
   def navbar_home_change
-    "navbar-transparent" if is_home
+    if is_home
+      "hidden"
+    else
+      "navbar-wagon-fixed"
+    end
+  end
+
+  def hide_search_bar
+    "hidden" if user_credential_pages
   end
 
   private
-    def is_home
-      controller_name == "pages" && action_name =="home"
-    end
+
+  def user_credential_pages
+    controller_name == 'devise/session' || 'devise/passwords' || 'devise/registration'
+  end
+
+  def is_home
+    controller_name == "pages" && action_name == "home"
+  end
+
 end
