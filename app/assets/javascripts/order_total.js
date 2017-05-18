71a6bb59d6ee847964d92e594d6b1a04daa47832
christian villaescusa <christian.villaescusa@gmail.com>
2017-05-18 19:06:40 +0200

    Updation order controller and gear show

--- app/assets/javascripts/order_total.js
+++ app/assets/javascripts/order_total.js
@@ -1,7 +1,26 @@
 var start_at = $('#start').val();
 var end_at = $('#end_at').val();
 var price = $('#price').val();
+var total = price * (( end_at - start_at ) + 1);
+
+$('#total').val(total);
+
+$( document ).ready(function() {
 
 
-$('#total').val(price * (( end_at - start_at ) + 1));
+$( "#start" ).change(function() {
+
+
+var start_at = $('#start').val();
+var end_at = $('#end_at').val();
+var price = $('#price').val();
+
+
+ alert( start_at );
+
+});
+$( "#start" ).change(function() {
+  alert( "Handler for .change() called." );
+});
+});
 
