27348c9c1b23be5552b4d1f20a4506745dc5ff18
Michael Ramassamy <ramassamy2000@gmail.com>
2017-05-18 12:18:16 +0200

    update search logic by searching according to city location

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -5,7 +5,7 @@ class GearsController < ApplicationController
   def index
     @gears = Gear.select('gears.*')
                 .joins(:size)
-                .where('sizes.category_id = ?' ,search_params[:category_id])
+                .where('sizes.category_id = ?' ,search_params[:category_id]).near(search_params[:city], 20)
     @category = Category.find(search_params[:category_id]).name
   end
 
