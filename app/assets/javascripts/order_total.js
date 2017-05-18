648fbc5dc0d53be16607e17f75c4cff70535ac96
christian villaescusa <christian.villaescusa@gmail.com>
2017-05-18 17:48:30 +0200

    updating price

--- app/assets/javascripts/order_total.js
+++ app/assets/javascripts/order_total.js
@@ -1,35 +1,31 @@
-function compute_price() {
+// function compute_price() {
 
-  var start_at = $('#start').val();
-  var end_at = $('#end').val();
-  var price = $('#price').val();
+//   var start_at = $('#start').val();
+//   var end_at = $('#end').val();
+//   var price = $('#price').val();
 
-  alert(start_at);
+//   alert(start_at);
 
-  if (start_at == ' ' || end_at == ' '){
-    return false;
-  }
-  else
-    return $('#total').val(price * (( end_at - start_at ) + 1)
-    );
-}
+//   if (start_at == ' ' || end_at == ' '){
+//     return false;
+//   }
+//   else
+//     return $('#total').val(price * (( end_at - start_at ) + 1)
+//     );
+// // }
 
-$(document).ready(function() {
 
-$('#sandbox-container input').datepicker({
-    format: "dd/mm/yyyy",
-    clearBtn: true,
-    language: "fr"
-});
+// $(document).ready(function() {
 
- $(#start).change(function() {
-  alert( "START Handler for .change() called." );
-  compute_price();
-})
-  $( "#end" ).change(function() {
-  alert( "END Handler  for .change() called." );
-  compute_price();
-})
-});
+
+//  $(#start).change(function() {
+//   alert( "START Handler for .change() called." );
+//   // compute_price();
+// })
+//   $( "#end" ).change(function() {
+//   alert( "END Handler  for .change() called." );
+//   // compute_price();
+// })
+// });
 
 
